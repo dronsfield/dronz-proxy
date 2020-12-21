e915375fa29025cab95af7bb38b3974382c0b210
Josh Dronsfield <josh.dronsfield@gmail.com>
2020-12-21 14:54:34 +0000

    tidy

--- index.js
+++ index.js
@@ -3,70 +3,19 @@ const port = process.env.PORT || 8080
 
 const ALLOWED_HOSTS = ["fantasy.premierleague.com"]
 
-// const getProxyForUrl = require('proxy-from-env').getProxyForUrl;
-// const corsProxy = require('cors-anywhere');
-// corsProxy.createServer({
-//     getProxyForUrl: (url) => {
-
-//     }
-//     // originWhitelist: ["https://fantasy.premierleague.com"],
-// }).listen(port, host, function() {
-//     console.log('Running CORS Anywhere on ' + host + ':' + port);
-// });
-
-// const Koa = require("koa")
-// const route = require("koa-route")
-// const url = require("url")
-// const fetch = require("isomorphic-unfetch")
-// const proxy = require("koa-proxy")
-
-// const server = new Koa()
-
-// server.use(async (ctx, next) => {
-//   const { path, href, host, hostname, origin, headers } = ctx
-//   console.log({ path, href, host, hostname, origin })
-//   // throw new Error("huh")
-//   if (path === "/") {
-//     ctx.body = "Hello there"
-//     return
-//   } else {
-//     const proxyUrl = url.parse(path.slice(1))
-//     console.log({ proxyUrl })
-//     // ctx.body = "proxying, are we?"
-//     if (ALLOWED_HOSTS.includes(proxyUrl.host)) {
-//       // ctx.body = "proxy away sir"
-//       console.log({ proxyUrl })
-//       // const resp = await fetch(proxyUrl.href, { headers })
-//       // // const text = await resp.text()
-//       // console.log(resp)
-//       // const data = await resp.json()
-//       // ctx.body = data
-//       console.log("um")
-//       return proxy({
-//         url: "https://" + proxyUrl.host + proxyUrl.path,
-//         requestOptions: { strictSSL: false, followRedirect: true }
-//       })(ctx, next)
-//     } else {
-//       ctx.throw(403)
-//     }
-//   }
-// })
-
-const express = require("express")
-const server = express()
-
+const server = require("express")()
 const proxy = require("http-proxy").createProxyServer()
 const url = require("url")
+
+server.use(require("compression")())
 
 server.get("*", function (req, res) {
   const path = req.path
   if (path === "/") {
-    res.send("Hello there")
-    return
+    return res.send("Hello there")
   } else {
     const proxyUrl = url.parse(path.slice(1))
     if (ALLOWED_HOSTS.includes(proxyUrl.host)) {
-      console.log("um")
       return proxy.web(req, res, {
         target: "https://" + proxyUrl.host + proxyUrl.path,
         changeOrigin: true,
@@ -74,9 +23,13 @@ server.get("*", function (req, res) {
       })
     } else {
       res.statusCode = 403
-      res.send(`host ${proxyUrl.host} not allowed`)
+      return res.send(`host ${proxyUrl.host} not allowed`)
     }
   }
+})
+
+app.use(function (err, req, res, next) {
+  res.status(500).send("nah")
 })
 
 server.listen(port, () => {
